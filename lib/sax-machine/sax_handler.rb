468f61953d0e4b89a7460c40e92f6c563dfc3caa
Mathias Biilmann Christensen <mbc@mbcbook.local>
2009-12-09 23:06:13 +0100

    Small refactorings of sax_handler.rb

--- lib/sax-machine/sax_handler.rb
+++ lib/sax-machine/sax_handler.rb
@@ -13,35 +13,32 @@ module SAXMachine
       object, config, value = stack.last
       value << string
     end
-
-    def cdata_block(string)
-      characters(string)
-    end
+    alias :cdata_block :characters
 
     def start_element(name, attrs = [])
       object, config, value = stack.last
-      sax_config = object.class.respond_to?(:sax_config) ? object.class.sax_config : nil
+      return unless object.class.respond_to?(:sax_config)
+      
+      sax_config = object.class.sax_config
 
-      if sax_config
-        if collection_config = sax_config.collection_config(name, attrs)
-          stack.push [object = collection_config.data_class.new, collection_config, ""]
-          object, sax_config, is_collection = object, object.class.sax_config, true
+      if collection_config = sax_config.collection_config(name, attrs)
+        stack.push [object = collection_config.data_class.new, collection_config, ""]
+        object, sax_config = object, object.class.sax_config
+      end
+      sax_config.element_configs_for_attribute(name, attrs).each do |ec|
+        unless parsed_config?(object, ec)
+          object.send(ec.setter, ec.value_from_attrs(attrs))
+          mark_as_parsed(object, ec)
         end
-        sax_config.element_configs_for_attribute(name, attrs).each do |ec|
-          unless parsed_config?(object, ec)
-            object.send(ec.setter, ec.value_from_attrs(attrs))
-            mark_as_parsed(object, ec)
-          end
-        end
-        if !collection_config && element_config = sax_config.element_config_for_tag(name, attrs)
-          stack.push [element_config.data_class ? element_config.data_class.new : object, element_config, ""]
-        end
+      end
+      if !collection_config && ec = sax_config.element_config_for_tag(name, attrs)
+        stack.push [ec.data_class ? ec.data_class.new : object, ec, ""]
       end
     end
 
     def end_element(name)
-      (object, tag_config, _), (element, config, value) = stack[-2..-1]
-      return unless stack.size > 1 && config && config.name.to_s == name.to_s
+      (object, _, _), (element, config, value) = stack[-2..-1]
+      return unless config && config.name.to_s == name.to_s
 
       unless parsed_config?(object, config)
         if config.respond_to?(:accessor)
